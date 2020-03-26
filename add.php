d3fc4af0f231263bbcfb54e101801ab5440d9ed1
Alex Jennings <jenningsalexyt@gmail.com>
2020-03-26 10:04:17 +0000

    changed functions and improved layout

--- add.php
+++ add.php
@@ -2,34 +2,34 @@
 
 require_once "functions.php";
 
-if (empty($_POST["brand"])){
-    $error=0;
+if (empty($_POST["brand"])) {
+    $error = 0;
 }
 
-if (empty($_POST["style"])){
-    $error=1;
+if (empty($_POST["style"])) {
+    $error = 1;
 }
 
-if (empty($_POST["instrument_url"])){
-    $error=2;
+if (empty($_POST["instrument_url"])) {
+    $error = 2;
 }
 
-if (empty($_POST["year"])){
-    $error=3;
+if (empty($_POST["year"])) {
+    $error = 3;
 }
 
-if ($_POST["year"]>2020){
-    $error=4;
+if ($_POST["year"]>2020) {
+    $error = 4;
 }
 
-if ($_POST["year"]<0){
-    $error=5;
+if ($_POST["year"]<0) {
+    $error = 5;
 }
 
-if (isset($error)){
-    header("Location: addguitars.php?error=". $error);
+if (isset($error)) {
+    header("Location: addguitars.php?error=" . $error);
     die();
-}else{
+} else {
     $db = getGuitarDatabase();
     addGuitarToDatabase($db, $_POST);
 
